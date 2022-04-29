4eeb490098567eec06911bdf37f306e32fc786cf
zhangyongsheng <864079692@qq.com>
2022-04-29 15:49:59 +0800

    创建子应用

--- webproject/webproject/settings.py
+++ webproject/webproject/settings.py
@@ -29,7 +29,7 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
-
+#注册apps
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 'book',
+    'book.apps.BookConfig',
 ]
 
 MIDDLEWARE = [
