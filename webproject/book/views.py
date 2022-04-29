fb85281427a5622d11d2c4b711f4124814ae0583
zhangyongsheng <864079692@qq.com>
2022-04-29 16:44:32 +0800

    视图函数

--- webproject/book/views.py
+++ webproject/book/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+'''
+view 函数
+1、接受请求
+2、返回响应 HttpResponse
+'''
 # Create your views here.
+def index(request):
+
+
+    return HttpResponse("VIEW函数，返回HttpResponse")
