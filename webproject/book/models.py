0ae0f9be49bc3f697380ab334f189ebc3bc3aade
zhangyongsheng <864079692@qq.com>
2022-04-30 11:32:01 +0800

    模型类，建表，迁移

--- webproject/book/models.py
+++ webproject/book/models.py
@@ -7,10 +7,30 @@ from django.db import models
 3、字段：
     字段名 = model.类型(选项)
     字段名不要用关键字，class def 等
+    不要用连续的下划线
+4、类型：
+    CharField
+    BooleanField
+    AutoField
+    DateField
+5、限制条件：
+    lenth
+    是否唯一 unique
+    是否为空 null  blank
+6、修改表名称
+    
 '''
 class Book(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100,unique=True)
     author = models.CharField(max_length=100)
+    pub_date = models.DateField(null=True)
+    readcount = models.BigIntegerField(default=0)
+    commentcount = models.BigIntegerField(default=0)
+    is_delete = models.BooleanField(default=False)
+
+    class Meta:
+        db_table = 'Bookinfo'
+        verbose_name = '书籍管理'
 
     def __str__(self):
         #显示书籍名字
